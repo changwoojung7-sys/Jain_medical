dcc16a40db4da2d044f0299a2ace075b7b6bcb12
Changwoo Jung <changwoojung7@gmail.com>
2026-01-07 13:04:17 +0900

    Delete src directory

--- src/config/siteConfig.js
+++
@@ -1,9 +0,0 @@
-export const siteConfig = {
-  companyName: "자인메디컬",
-  ceoName: "윤석신",
-  phone: "02-454-9200", // 샘플
-  email: "suksiny@naver.com", // 샘플
-  address: "경기 구리시 우미내길 12-21", // 샘플
-  businessHours: "평일 09:00 - 18:00",
-  tagline: "임상병리 현장을 가장 잘 아는 진단 소모품 파트너",
-};
